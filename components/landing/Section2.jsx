074eab7cda8f2c55ffe029619912021d88a410c6
SahilSharma2911 <sahilsharmarksh@gmail.com>
2024-04-09 21:56:06 +0530

    new change

--- components/landing/Section2.jsx
+++ components/landing/Section2.jsx
@@ -12,10 +12,10 @@ const Section2 = () => {
         <div className="flex flex-col items-start justify-center">
           <h3 className="font-semibold text-[32px]">Chesper by Cacoona</h3>
           <p className="text-[20px] leading-[160%] mt-2">
-            A Chesper is your paper chest. It's where you can be free to imagine
+            A Chesper is your paper chest. It&apos;s where you can be free to imagine
             and record your story, outline your journey and display your inner
             youth. Only good things and positive thoughts go into your chesper
-            about you. There's always room for growth within you but to do so
+            about you. There&apos;s always room for growth within you but to do so
             you must relinquish your burdens, anger and fears and there are
             specific pages for those moments. Your Chesper is a safe for your
             secrets and deep reflections.
@@ -28,7 +28,7 @@ const Section2 = () => {
           <p className="text-[20px] leading-[160%] mt-2">
             Elevate and connect your style with the celestial realm through
             Cacoona Charms and Accessories. Our astrology-inspired charms and
-            accessories are more than mere adornments; they’re symbols of cosmic
+            accessories are more than mere adornments; they&apos;re symbols of cosmic
             alignment and spiritual empowerment. Each piece is thoughtfully
             designed to resonate with your unique astrological energy, allowing
             you to harness the power of the universe and express your inner
@@ -45,7 +45,7 @@ const Section2 = () => {
             Journey with us into the future with Cacoona your exclusive preview
             of upcoming products and releases. Be the first to discover our
             later expansions as we gain insider access to our creative process
-            and products development ensuring that you’re always at the
+            and products development ensuring that you&apos;re always at the
             forefront of the Cacoona experience
           </p>
         </div>
